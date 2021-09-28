e45f3ff9a359214979554ed1d7a4c26d3a71abb9
H37kouya <h37kouya@gmail.com>
2021-09-28 15:26:00 +0900

    fix

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -5,5 +5,5 @@ alias rm='rm -i'
 
 # tree
 if [ -x /usr/bin/tree ]; then
-    alias tree = pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
+    alias tree="pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'"
 fi
