9c0a71650e98de05685d215c93d6e1cae1ee6700
H37kouya <h37kouya@gmail.com>
2021-09-28 15:24:25 +0900

    add tree コマンド

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,3 +2,8 @@
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
+
+# tree
+if [ -x tree ]; then
+    alias tree = pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/|  /g'
+fi
